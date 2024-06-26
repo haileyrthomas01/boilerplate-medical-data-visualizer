b71f93aa05cbdfe21833075b755165155aed7da8
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2024-06-26 10:32:30 -0500

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,55 +4,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
 
 # 3
-
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature.
+    df_cat = df_cat.rename(columns={'variable': 'Variable', 'value': 'Value'})
 
-    # 6
-    df_cat = None
+    # Draw the catplot with 'sns.catplot()'
+    cat_plot = sns.catplot(x='Variable', hue='Value', col='cardio', data=df_cat, kind='count', order=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+    cat_plot.set_axis_labels("variable", "total")
+
+    # Adjust the following line if necessary to save the figure
+    cat_plot.savefig('catplot.png')
     
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
-    fig.savefig('catplot.png')
-    return fig
+    # Return the figure that is embedded in the FacetGrid
+    return cat_plot.fig
 
 
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(11, 9))
 
     # 15
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', ax=ax, cmap='coolwarm', linewidths=1, linecolor='black')
 
 
     # 16
